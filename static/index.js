eb20fa9a7d47b8e60e1ee94c4d7c3e4b1f137057
yeetman123447-web <yeetman123447@gmail.com>
2026-03-23 08:10:29 -0500

    Update index.js

--- static/index.js
+++ static/index.js
@@ -455,17 +455,10 @@ const openNewtab = () => {
 };
 
 let splashtext = [
-  "check out discord.gg/goabyss",
-  "made by paxton warin",
-  "want your own link? check out discord.gg/goabyss",
-  "thanks for using the site!",
-  "check out the settings page",
-  "check out the games page",
-  "check out the settings to hide from your teacher",
-  "need ChatGPT? search for it on the games page!",
-  "shoutout owen, ramsey, and levi",
-  "co owned by wyatt batdorf",
-  "enzo is interesting...",
+  "code like a pro 🔥🔥🔥",
+  "coding was never easier!",
+  "now even you could code!",
+  "Soooo pro",
 ]
 
 const runService = async (url, override, overrideadrbar) => {
